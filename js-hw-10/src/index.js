036fce110da07913550a252fd60df407d1cd6bcc
Konstantin Bondarenko <43937942+Konstantin84UKR@users.noreply.github.com>
2021-04-04 20:50:07 +0300

    Delete js-hw-10 directory

--- js-hw-10/src/index.js
+++
@@ -1,4 +0,0 @@
-import './styles.css';
-import './checkBox.js';
-import './menu.js';
-console.log('Web Pack');
--- js-hw-10/src/menu.js
+++
@@ -1,6 +0,0 @@
-import data from "./menu.json";
-import template from "./template.hbs";
-
-const menuHTML = document.querySelector(".js-menu");
-const menu = template(data);
-menuHTML.innerHTML = menu;
