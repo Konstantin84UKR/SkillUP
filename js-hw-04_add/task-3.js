6e0590b4655f868cb32276d11704986d56b39de2
Konstantin Bondarenko <43937942+Konstantin84UKR@users.noreply.github.com>
2021-04-04 20:49:18 +0300

    Delete js-hw-04_add directory

--- js-hw-04_add/task-3.js
+++
@@ -1,9 +0,0 @@
-//Створити метод compact який приймає масив і
-//вертає новий де відсутні будь-які значення що при переведені в bool дають false
-
-
-function compact(arr){
-   return  arr.filter(x => Boolean(x));
-}
-
-console.log(compact([1,0,'', null, 'Hello'])); // [1,'Hello']
